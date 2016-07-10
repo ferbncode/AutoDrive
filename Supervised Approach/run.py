4571a15ac7a8518a9ad9344d012ee28482b0b290
ferbncode <suyashgargsfam@gmail.com>
2016-07-11 00:12:30 +0530

    Completed Basic App

--- Supervised Approach/run.py
+++ Supervised Approach/run.py
@@ -9,11 +9,22 @@ from captureExtractFeatures import *
 X_train, X_test, y_train, y_test = train_test(X, y)
 clf = DecisionTreeClassifier(max_depth=3, random_state=35)
 clf = train_classifier(clf, X_train, y_train)
+# the classifier has a 0.77 f1 score on the test set. (can be improved by using sophisticated hardware for
+# sensors and better training set
+totalTime = int(raw_input("Enter the amount for which the agent should try to perform\
+ in the environment: "))
 
-totalTime = raw_input("Enter the amount for which the agent should try to perform\
- in the environment: ")
+for time_unit in range(totalTime):
+	#incomplete
+	captureImage()
+	imgArray = convertToKClusteredImage('photo.jpg')
+	im = make2dBlueMatrix(imgArray)
 
-#incomplete
+	pred_action = predict_label(clf, im)
+
+	print pred_action
+
+	#takeAction(action = pred_action)
 
 
 
