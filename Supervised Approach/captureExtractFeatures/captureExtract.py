1c6959e01dedeaf046a91fe692010ec92e15a3ca
ferbncode <suyashgargsfam@gmail.com>
2016-07-11 00:11:44 +0530

    Updated captureImage

--- Supervised Approach/captureExtractFeatures/captureExtract.py
+++ Supervised Approach/captureExtractFeatures/captureExtract.py
@@ -7,11 +7,12 @@ import Image
 
 
 
-def convertToKClusterdImage(img):
-	system("convert {} -resize 30x28! {}.png".format(img))
+def convertToKClusteredImage(img):
+	print img
+	system("convert {} -resize 30x28! {}.png".format(img, img))
 	# figured out the worst method but the easiest implementation
-	a, b, c = cv2.imread(img).shape
-	im = cv2.imread(img).transpose(2,0,1).reshape(3, -1).transpose()
+	a, b, c = cv2.imread('{}.png'.format(img)).shape
+	im = cv2.imread('{}.png'.format(img)).transpose(2,0,1).reshape(3, -1).transpose()
 	# Don't ask. I know its shit code
 	clusterer = KMeans(n_clusters=2, max_iter=50, random_state=1)
 	preds = clusterer.fit_predict(im)
@@ -20,16 +21,22 @@ def convertToKClusterdImage(img):
 	im = np.fliplr(im)
 	im = im.reshape(a, b, c)
 	img = Image.fromarray(im)
-	Image.save('img_clustered.png')
+	#Image.save('img_clustered.png')
+	print im.shape
 	return im
 
 def make2dBlueMatrix(img):
-	imgBlueIntensities = img.transpose(2, 0, 1).reshape(3, -1)[0]
+	print img.shape
+	imgBlueIntensities = img.transpose(2, 0, 1).reshape(3, -1)
+	print imgBlueIntensities.shape
 	return imgBlueIntensities[0]
 
 def captureImage():
-	system("wget http://192.168.43.1:8080/photo.jpg")
-
-
+	url = "http://192.168.43.134:8080/photo.jpg"
+	response = requests.get(url)
+	if response.status_code  == 200:
+		f= open("photo.jpg", 'wb')
+		f.write(response.content)
+		f.close()
 
 	
